4b5e8f9be5c956418569026cbe28fca592311ee3
CPatton2000 <70605880+CPatton2000@users.noreply.github.com>
2022-10-14 10:17:16 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,6 @@
+#I've done FizzBuzz literally like 6 times throughout college, so I know the process by heart. 
+#So, I hope you can excuse me combining the 'getting it to work' and the 'improving effeciency' parts because at this point I literally 
+#have no idea how to BS the code into looking bad for the sake of entertaining the 'getting it to work' perameter.
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print ('FizzBuzz')

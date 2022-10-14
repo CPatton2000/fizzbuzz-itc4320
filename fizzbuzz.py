f3f85ba81a9b3ddb1ea01d9c0714c990b93a31c2
CPatton2000 <70605880+CPatton2000@users.noreply.github.com>
2022-10-14 10:03:26 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
+for i in range(1, 101): #Selects a range between 2 numbers
+    if i % 3 == 0 and i % 5 == 0: #If the number is divisible by 3 and 5 without any remainder, then the program will print "FizzBuzz"
         print ('FizzBuzz')
-    elif i % 3 == 0:
+    elif i % 3 == 0: #If the number is divisible by 3 without a remainder, then the program will print "Fizz"
         print ('Fizz')
-    elif i % 5 == 0:
+    elif i % 5 == 0: #If the number is divisible by 5 without a remainder, then the program will print "Fizz"
         print ('Buzz')
     else:
-        print (str(i))
+        print (str(i)) #prints the number if it is not cleanly divisible by 3 or 5
